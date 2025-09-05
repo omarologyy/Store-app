eb4af9935a5df64ab8b2fdbbf0807a3924c7167c
Umar Sulaiman <akoredeumar8@gmail.com>
2025-09-05 18:26:44 +0100

    Setup admin structure and restrict access to admin pages

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductPage() {
+  return <div>Admin Products Page</div>;
+}
+
+export default ProductPage;
--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about"]);
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
+  const { userId } = await auth();
+  const isAdminUser = userId === process.env.ADMIN_USER_ID;
+
+  if (isAdminRoute(req) && !isAdminUser) {
+    return NextResponse.redirect(new URL("/", req.url));
   }
+  if (!isPublicRoute(req)) auth.protect();
 });
 
 export const config = {
