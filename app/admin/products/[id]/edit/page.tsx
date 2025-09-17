7ad0e11eaa865d70bee66929877afaae3b31fe6b
Umar Sulaiman <akoredeumar8@gmail.com>
2025-09-17 17:34:33 +0100

    Image update on supabase and frontend synced successfully

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -5,6 +5,8 @@ import PriceInput from "@/components/form/PriceInput";
 import TextAreaInput from "@/components/form/TextAreaInput";
 import { SubmitButton } from "@/components/form/Buttons";
 import CheckboxInput from "@/components/form/CheckBoxInput";
+import ImageInputContainer from "@/components/form/ImageInputContainer";
+import { updateProductImageAction } from "@/utils/actions";
 async function EditProductPage({ params }: { params: { id: string } }) {
   const { id } = params;
   const product = await fetchAdminProductDetails(id);
@@ -13,7 +15,15 @@ async function EditProductPage({ params }: { params: { id: string } }) {
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">update product</h1>
       <div className="border p-8 rounded-md">
-        {/* Image Input Container */}
+        <ImageInputContainer
+          action={updateProductImageAction}
+          name={name}
+          image={product.image}
+          text="update image"
+        >
+          <input type="hidden" name="id" value={id} />
+          <input type="hidden" name="url" value={product.image} />
+        </ImageInputContainer>
         <FormContainer action={updateProductAction}>
           <div className="grid gap-4 md:grid-cols-2 my-4">
             <input type="hidden" name="id" value={id} />
